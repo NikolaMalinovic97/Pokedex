406f6515810a73161dcda84f414f9a300ac1ae71
Nikola <nikolamalinovic97@gmail.com>
2020-05-05 21:43:38 +0200

    Add routing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,21 @@
+import {autoinject} from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { PLATFORM } from 'aurelia-pal';
+
+@autoinject
 export class App {
-  public message: string = 'Hello World!';
+
+  constructor(private router: Router) {}
+
+  configureRouter(config, router) {
+    this.router = router;
+    config.title = 'Pokedex';
+    config.options.pushState = true;
+    config.options.root = '/';
+    config.map([
+      { route: ['', 'pokemon'], name: 'pokemon', moduleId: PLATFORM.moduleName("components/pokemon-list/pokemon-list"), title: 'Pokemon list', nav: true },
+      { route: 'test', name: 'test', moduleId: PLATFORM.moduleName("components/test-component/test-component"), title: 'Test', nav: true },
+      { route: '*path', name: 'home', moduleId: PLATFORM.moduleName("components/not-found/not-found.html") }
+    ]);
+  }
 }
--- src/components/header/header.ts
+++ src/components/header/header.ts
@@ -1,5 +1,13 @@
+import {autoinject} from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+
+@autoinject
 export class Header {
 
-  constructor() {
+  constructor(private router: Router) {
+  }
+
+  attached() {
+    console.log(this.router.navigation);
   }
 }
