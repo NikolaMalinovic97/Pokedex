ea9f53c9c443c3145c9592c3c8b989023ba4c816
Nikola <nikolamalinovic97@gmail.com>
2020-06-02 14:54:38 +0200

    Add internationalization and translation.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as environment from '../config/environment.json';
 import {PLATFORM} from 'aurelia-pal';
 import { AureliaConfiguration } from 'aurelia-configuration';
 import * as Toastr from 'toastr';
+import { I18N } from 'aurelia-i18n';
+import Backend from 'i18next-xhr-backend';
 
 Toastr.options = {
   "closeButton": false,
@@ -28,7 +30,21 @@ export function configure(aurelia: Aurelia) {
     .feature(PLATFORM.moduleName('resources/index'))
     // .plugin(PLATFORM.moduleName('aurelia-configuration'));
     .plugin(PLATFORM.moduleName('aurelia-dialog'))
-    .plugin(PLATFORM.moduleName('aurelia-validation'));
+    .plugin(PLATFORM.moduleName('aurelia-validation'))
+    .plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+      instance.i18next.use(Backend);
+      return instance.setup({
+        backend: {
+          loadPath: 'locale/{{lng}}/{{ns}}.json',
+        },
+        ns: ['translation'],
+        defaultNS: 'translation',
+        lng: localStorage.getItem('language'),
+        attributes: ['i18n'],
+        fallbackLng: 'en',
+        debug: false
+      });
+    })
 
   aurelia.use.developmentLogging(environment.debug ? 'debug' : 'warn');
 
